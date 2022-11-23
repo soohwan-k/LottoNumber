5118177b252e04eacb3b9899a20a848e5600b571
SooHwan <newpoo@naver.com>
2022-11-23 18:47:39 +0900

    feat: 번호 background 추가

--- app/src/main/java/org/tech/town/lottonumber/MainActivity.kt
+++ app/src/main/java/org/tech/town/lottonumber/MainActivity.kt
@@ -7,6 +7,7 @@ import android.widget.Button
 import android.widget.NumberPicker
 import android.widget.TextView
 import android.widget.Toast
+import androidx.core.content.ContextCompat
 import androidx.core.view.isVisible
 
 class MainActivity : AppCompatActivity() {
@@ -69,9 +70,10 @@ class MainActivity : AppCompatActivity() {
 
                 textView.text = number.toString()
                 textView.isVisible = true
+
+                setNumberBackground(number, textView)
             }
 
-            Log.d("MainActivity", list.toString())
         }
     }
 
@@ -99,10 +101,23 @@ class MainActivity : AppCompatActivity() {
             textView.isVisible = true
             textView.text = numberPicker.value.toString()
 
+            setNumberBackground(numberPicker.value, textView)
+
+
             pickNumberSet.add(numberPicker.value)
         }
     }
 
+    private fun setNumberBackground(number: Int, textView: TextView) {
+        when(number){
+            in 1..10 -> textView.background = ContextCompat.getDrawable(this, R.drawable.circle_yellow)
+            in 11..20 -> textView.background = ContextCompat.getDrawable(this, R.drawable.circle_blue)
+            in 21..30 -> textView.background = ContextCompat.getDrawable(this, R.drawable.circle_red)
+            in 31..40 -> textView.background = ContextCompat.getDrawable(this, R.drawable.circle_green)
+            else -> textView.background = ContextCompat.getDrawable(this, R.drawable.circle_gray)
+        }
+
+    }
     private fun initClearButton() {
         clearButton.setOnClickListener {
             pickNumberSet.clear()
